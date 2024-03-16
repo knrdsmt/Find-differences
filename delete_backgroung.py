ae4a78536db72cf0a53df7ed6897ea0553857521
knrdsmt <97449172+knrdsmt@users.noreply.github.com>
2024-03-17 00:33:44 +0100

    Cutting differences

--- delete_backgroung.py
+++ delete_backgroung.py
@@ -1,70 +1,70 @@
 import cv2
 import numpy as np
 
-obraz1 = cv2.imread('dublin.jpg')
-obraz2 = cv2.imread('dublin_edited.jpg')
+image1 = cv2.imread('dublin.jpg')
+image2 = cv2.imread('dublin_edited.jpg')
 
-roznica = np.abs(obraz1.astype(int) - obraz2.astype(int)).astype(np.uint8)
-roznica_szarosc = np.dot(roznica[..., :3], [0.2989, 0.5870, 0.1140])
-prog = (roznica_szarosc > 30) * 255
+difference = np.abs(image1.astype(int) - image2.astype(int)).astype(np.uint8)
+gray_difference = np.dot(difference[..., :3], [0.2989, 0.5870, 0.1140])
+threshold = (gray_difference > 30) * 255
 
-kontury = []
-for y in range(prog.shape[0]):
-    for x in range(prog.shape[1]):
-        if prog[y, x] == 255:
-            kontury.append((x, y))
+contours = []
+for y in range(threshold.shape[0]):
+    for x in range(threshold.shape[1]):
+        if threshold[y, x] == 255:
+            contours.append((x, y))
 
-obiekty = []
-for kontur in kontury:
-    dodano = False
-    for obiekt in obiekty:
-        if min([np.sqrt((x - kontur[0]) ** 2 + (y - kontur[1]) ** 2) for x, y in obiekt]) < 50:
-            obiekt.append(kontur)
-            dodano = True
+objects = []
+for contour in contours:
+    added = False
+    for obj in objects:
+        if min([np.sqrt((x - contour[0]) ** 2 + (y - contour[1]) ** 2) for x, y in obj]) < 50:
+            obj.append(contour)
+            added = True
             break
-    if not dodano:
-        obiekty.append([kontur])
+    if not added:
+        objects.append([contour])
 
-for obiekt in obiekty:
-    x_min = min(obiekt, key=lambda x: x[0])[0]
-    y_min = min(obiekt, key=lambda x: x[1])[1]
-    x_max = max(obiekt, key=lambda x: x[0])[0]
-    y_max = max(obiekt, key=lambda x: x[1])[1]
+for obj in objects:
+    x_min = min(obj, key=lambda x: x[0])[0]
+    y_min = min(obj, key=lambda x: x[1])[1]
+    x_max = max(obj, key=lambda x: x[0])[0]
+    y_max = max(obj, key=lambda x: x[1])[1]
 
-    obraz2[y_min:y_max, x_min] = [0, 255, 0]
-    obraz2[y_min:y_max, x_max] = [0, 255, 0]
-    obraz2[y_min, x_min:x_max] = [0, 255, 0]
-    obraz2[y_max, x_min:x_max] = [0, 255, 0]
+    image2[y_min:y_max, x_min] = [0, 255, 0]
+    image2[y_min:y_max, x_max] = [0, 255, 0]
+    image2[y_min, x_min:x_max] = [0, 255, 0]
+    image2[y_max, x_min:x_max] = [0, 255, 0]
 
-cv2.imwrite('dublin_bbox.jpg', obraz2)
+cv2.imwrite('dublin_bbox.jpg', image2)
 
-najwiekszy_obiekt = max(obiekty, key=len)
+largest_object = max(objects, key=len)
 
-# Wyznacz bounding box dla największego obiektu
-x_min = min(najwiekszy_obiekt, key=lambda x: x[0])[0] + 1
-y_min = min(najwiekszy_obiekt, key=lambda x: x[1])[1] + 1
-x_max = max(najwiekszy_obiekt, key=lambda x: x[0])[0]
-y_max = max(najwiekszy_obiekt, key=lambda x: x[1])[1]
+# Calculate bounding box for the largest object
+x_min = min(largest_object, key=lambda x: x[0])[0] + 1
+y_min = min(largest_object, key=lambda x: x[1])[1] + 1
+x_max = max(largest_object, key=lambda x: x[0])[0]
+y_max = max(largest_object, key=lambda x: x[1])[1]
 
-wyciecie = obraz2[y_min:y_max, x_min:x_max]
-obraz3 = np.copy(wyciecie)
-wyciecie1 = obraz1[y_min:y_max, x_min:x_max]
-wyciecie2 = obraz2[y_min:y_max, x_min:x_max]
+cropped_image = image2[y_min:y_max, x_min:x_max]
+image3 = np.copy(cropped_image)
+cropped_image1 = image1[y_min:y_max, x_min:x_max]
+cropped_image2 = image2[y_min:y_max, x_min:x_max]
 
-roznica_wyciecia = np.abs(wyciecie1.astype(int) - wyciecie2.astype(int)).astype(np.uint8)
-roznica_wyciecia_szarosc = np.dot(roznica_wyciecia[..., :3], [0.2989, 0.5870, 0.1140])
-prog_wyciecia = (roznica_wyciecia_szarosc > 7) * 255
+difference_cropped = np.abs(cropped_image1.astype(int) - cropped_image2.astype(int)).astype(np.uint8)
+gray_difference_cropped = np.dot(difference_cropped[..., :3], [0.2989, 0.5870, 0.1140])
+threshold_cropped = (gray_difference_cropped > 7) * 255
 
-roznice = []
-for y in range(prog_wyciecia.shape[0]):
-    for x in range(prog_wyciecia.shape[1]):
-        if prog_wyciecia[y, x] == 255:
-            roznice.append((x, y))
+differences = []
+for y in range(threshold_cropped.shape[0]):
+    for x in range(threshold_cropped.shape[1]):
+        if threshold_cropped[y, x] == 255:
+            differences.append((x, y))
 
-maska = np.zeros_like(obraz3[:, :, 0], dtype=np.uint8)
-for roznica in roznice:
-    maska[roznica[1], roznica[0]] = 1
+mask = np.zeros_like(image3[:, :, 0], dtype=np.uint8)
+for difference in differences:
+    mask[difference[1], difference[0]] = 1
 
-obraz3_rgba = np.dstack((obraz3, np.full(obraz3.shape[:2], 255)))
-obraz3_rgba[maska != 1] = [0, 0, 0, 0]
-cv2.imwrite('bbox_bez_tla.png', obraz3_rgba)
+image3_rgba = np.dstack((image3, np.full(image3.shape[:2], 255)))
+image3_rgba[mask != 1] = [0, 0, 0, 0]
+cv2.imwrite('bbox_without_background.png', image3_rgba)
